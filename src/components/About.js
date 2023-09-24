74b39f72e752fbb9144ce8f72ca48924708347eb
AhmedElshenawy0 <shenawy100@gmail.com>
2023-09-24 17:26:45 +0200

    update

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,7 @@ export default function About() {
           <section className="col-lg-6 hero-text">
               <h1>Little Lemon</h1>
               <h2>Chicago</h2>
-              <p className="about-subtext">Little Lemon opened in 1995 by two Italian brothers, Adrian and Mario. 
+              <p className="about-subtext">Little Lemon Opened in 1995 by two Italian brothers, Adrian and Mario. 
               Despite the city's diversity, the two brothers recognized the lack of Mediterranean cuisine in Chicago, 
               and were inspired to bring the flavors of their hometown in Italy to the people of Chicago. The two brothers 
               continue to oversee the Little Lemon restaurant, nearly thirty years later.</p>
