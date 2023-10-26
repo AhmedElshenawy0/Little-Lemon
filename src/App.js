3379e315bce883eeb187f822a3cbce0a6ecc0d5c
AhmedElshenawy0 <shenawy100@gmail.com>
2023-10-26 23:39:15 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import './App.css';
-import Footer from './components/Footer';
-import Navbar from './components/Navbar';
-import Routing from './components/Routing';
-
+import "./App.css";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Homepage from "./HomePage";
+import About from "./About";
+import Menu from "./Menu";
+import BookingPage from "./BookingPage";
+import Order from "./Order";
+import Confirmation from "./Confirmation";
 function App() {
   return (
-    <>
-      <Navbar/>
-      <Routing/>
-      <Footer/>
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/menu" element={<Menu />} />
+
+        <Route path="/reservations" element={<BookingPage />} />
+
+        <Route path="/order" element={<Order />} />
+
+        <Route path="/confirmation" element={<Confirmation />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
--- src/components/About.js
+++ src/components/About.js
@@ -13,7 +13,7 @@ export default function About() {
               <p className="about-subtext">Little Lemon Opened in 1995 by two Italian brothers, Adrian and Mario. 
               Despite the city's diversity, the two brothers recognized the lack of Mediterranean cuisine in Chicago, 
               and were inspired to bring the flavors of their hometown in Italy to the people of Chicago. The two brothers 
-              continue to oversee the Little Lemon restaurant, nearly thirty years later.</p>
+              continue to oversee the Little Lemon restaurant, Nearly thirty years later.</p>
           </section>
 
           </div>
